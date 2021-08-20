9f228f0a2a7c6674b4fe4b45e2747c272616fe97
kingharong <91714@naver.com>
2021-08-20 15:12:11 +0900

    eslint 오류 해결

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+import Layout from 'Layout/Layout'
+
+const Login = () => {
+	return <Layout></Layout>
+}
+
+export default Login
