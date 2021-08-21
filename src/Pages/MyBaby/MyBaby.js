8d2e70c2c913c15b598025805ec459d4c9132768
YUZAMIN <untmd.crmn@gmail.com>
2021-08-22 01:16:10 +0900

    feat : Add PageTitles component

--- src/Pages/MyBaby/MyBaby.js
+++ src/Pages/MyBaby/MyBaby.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import styled from 'styled-components/macro'
 import Layout from 'Layout/Layout'
+import PageTitles from 'Components/PageTitles/PageTitles'
 
 export default function MyBaby() {
   return (
     <Layout>
-      <div>
+      <Container>
+        <PageTitles
+          title="내 새끼 자랑하기"
+          subtitle="소중한 반려동물을 자랑하는 공간입니다."
+        />
         <div>My baby</div>
         <div>My baby</div>
-      </div>
+      </Container>
     </Layout>
   )
 }
+const Container = styled.div`
+  min-width: 700px;
+`
