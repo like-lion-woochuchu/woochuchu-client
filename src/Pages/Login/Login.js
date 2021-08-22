c9b27b2dc1b4c934c2d6fc4c86c56d15d96c9cc2
kingharong <91714@naver.com>
2021-08-22 20:48:23 +0900

    feat : 구글&카카오 로그인

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,42 @@
 import React from 'react'
 import styled from 'styled-components/macro'
 import Layout from 'Layout/Layout'
+import loginFormLogo from 'Assets/Images/Logo/main-logo144px@2x.png'
+import GoogleSignIn from 'Components/Login/GoogleSignIn'
+import KakaoSignIn from 'Components/Login/KakaoSignIn'
 
 const Login = () => {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <LoginForm>
+        <LoginLogo></LoginLogo>
+        <GoogleSignIn></GoogleSignIn>
+        <KakaoSignIn></KakaoSignIn>
+      </LoginForm>
+    </Layout>
+  )
 }
+
+const LoginForm = styled.div`
+  margin-top: 43px;
+  margin-bottom: 47px;
+  left: 610px;
+  width: 700px;
+  height: 674px;
+  background: transparent url('https://ifh.cc/g/vghVIH.png') 0% 0% no-repeat
+    padding-box;
+  border-radius: 30px;
+  opacity: 1;
+`
+
+const LoginLogo = styled.img.attrs({
+  src: loginFormLogo,
+})`
+  margin-top: 60px;
+  margin-left: 278px;
+  width: 144px;
+  height: 144px;
+  opacity: 1;
+`
 
 export default Login
