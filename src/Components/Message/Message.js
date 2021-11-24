9794c0c49f1280b07fd9443ad27afe97dd8e5829
kingharong <91714@naver.com>
2021-11-24 12:42:10 +0900

    feat: 쪽지 발송 mock 서버와 통신

--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -9,10 +9,49 @@ import {
 import submitIcon from 'Assets/Icon/icon-arrow-right-white16px@2x.png'
 import { useState } from 'react'
 import axios from 'axios'
+import { useHistory } from 'react-router'
 
 const Message = () => {
   const [text, setText] = useState('')
-
+  const history = useHistory()
+  const submit = () => {
+    if (!localStorage.getItem('token')) {
+      alert('로그인이 필요합니다.')
+      history.push('/login')
+    } else if (!text) {
+      alert('쪽지 내용을 작성해 주세요.')
+    } else {
+      const token = localStorage.getItem('token')
+      axios
+        .post(
+          'https://58012740-20bb-4b6d-b6ae-dc77d28bb281.mock.pstmn.io/note/',
+          {
+            receiver: 11,
+            body: text,
+          },
+          {
+            headers: {
+              'Content-type': 'application/json',
+              Accept: 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((response) => {
+          if (response.status === 200) {
+            console.log(response)
+            alert('쪽지가 성공적으로 발송되었습니다.')
+          }
+        })
+        .catch((error) => {
+          if (error.response.request.status === 400) {
+            alert('오류가 발생했습니다. 다시 작성해 주세요.')
+          } else if (error.response.request.status === 500) {
+            alert('정상적인 접근이 아닙니다.')
+          }
+        })
+    }
+  }
   return (
     <>
       <MessageDiv height={'780px'}>
@@ -24,7 +63,7 @@ const Message = () => {
           placeholder={'쪽지 내용을 입력하세요.'}
           onChange={(e) => setText(e.target.value)}
         ></MessageTextFrame>
-        <SubmitButton>
+        <SubmitButton onClick={() => submit()}>
           보내기
           <SubmitIcon />
         </SubmitButton>
