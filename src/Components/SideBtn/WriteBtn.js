65f209c6fb6b57cadab172c289488f97ebfb772d
YUZAMIN <untmd.crmn@gmail.com>
2021-12-28 01:29:23 +0900

    feat : Change side write btn image

--- src/Components/SideBtn/WriteBtn.js
+++ src/Components/SideBtn/WriteBtn.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components/macro'
-import Write from 'Assets/Icon/button-scroll-top60px@2x.png'
+import Write from 'Assets/Icon/pen.png'
 import { ScrollButton } from './ScrollTopBtn'
 
 export default function WriteBtn({ handleClick }) {
